4c094e5ff985e7f3096743a7ef29f809869343b8
jisu youn <jisu.youn@nhn.com>
2013-12-12 20:22:40 +0900

    add leftMenu style setting function

--- _site/js/list.js
+++ _site/js/list.js
@@ -31,16 +31,14 @@ function attachEvents() {
         var weMatchedList = $Element($$.getSingle('.CAT_'+sCat));
         var elefoot = $Element($$.getSingle('footer'));
 
+
+        _setStyleMenu(weSelectedMenu);
+
+
         //매치된 리스트 영역 보여주기 
         weMatchedList.css({
           'display' : 'block'
         });
-
-        //좌측 선택된 메뉴 영역 하이라이트
-        weSelectedMenu.css({
-            'backgroundColor' : 'rgb(163, 163, 163)',
-            'color' : 'white'
-        })
 
         //매치된 리스트 영역의 크기 설정
         var eleHeight = parseInt(weMatchedList.css('height'));
@@ -68,6 +66,20 @@ function attachEvents() {
         if( nGap > 0) {
             elefoot.css({'top' : nGap + nFootMarginTop + "px"})
         }
+
+        //internal function
+        function _setStyleMenu(selectedEle) {
+            var weChilds = $Element(eEvent.element.parentElement).child();
+
+            for(var i = 0 , len = weChilds.length ; i <len ; i++) {
+                if(weChilds[i].$value() === selectedEle.$value()) {
+                    weChilds[i].css({ 'backgroundColor' : 'rgb(163, 163, 163)', 'color' : 'white' });
+                }else {
+                    weChilds[i].css({ 'backgroundColor' : 'white', 'color' : 'black' });
+                }
+            }
+        }
+
       }
   );
 

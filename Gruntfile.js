aa05ded3c292836745ee14b7682240f866d09f61
jisu youn <jisu.youn@nhn.com>
2014-03-12 16:44:59 +0900

    refactoring Dir and applied Grunt Module(merge&uglify)

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    //Project configuration
+    grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
+
+        dirs : {
+            src : 'src/',
+            dest : 'dest/js/<%= pkg.version %>',
+            destCss : 'dest/css/<%= pkg.version %>',
+        },
+
+        concat : {
+            options : {
+                //separator : ';',
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                           '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+            },
+            base : {
+                src : ["<%= dirs.src %>/lib/js/jindo_2.8.1.js","<%= dirs.src %>/js/list_rf.js"] ,
+                dest : '<%= dirs.dest %>/listPage.js',
+                nonull : true,
+            },
+            mobile : {
+                src : ["<%= dirs.src %>/lib/js/jindo_2.8.1.js","<%= dirs.src %>/js/list_m.js"] ,
+                dest : '<%= dirs.dest %>/listPage_m.js',
+                nonull : true,
+            },
+
+            cssIndex: {
+                src : ["<%= dirs.src %>/lib/css/reset.css","<%= dirs.src %>/css/common.css", "<%= dirs.src %>/css/main.css"] ,
+                dest : '<%= dirs.destCss %>/mainPage.css',
+                nonull : true,
+            },
+            cssPost: {
+                src : ["<%= dirs.src %>/lib/css/reset.css","<%= dirs.src %>/css/common.css", "<%= dirs.src %>/css/postLayout.css"] ,
+                dest : '<%= dirs.destCss %>/postLayoutPage.css',
+                nonull : true,
+            },
+            cssList : {
+                src : ["<%= dirs.src %>/lib/css/reset.css","<%= dirs.src %>/css/common.css", "<%= dirs.src %>/css/list.css"] ,
+                dest : '<%= dirs.destCss %>/listPage.css',
+                nonull : true,
+            },
+            cssListMobile : {
+                src : ["<%= dirs.src %>/lib/css/reset.css","<%= dirs.src %>/css/common.css", "<%= dirs.src %>/css/list_m.css"] ,
+                dest : '<%= dirs.destCss %>/listPage_m.css',
+                nonull : true,
+            }
+        },
+
+        uglify : {
+            my_target : {
+                options : {
+                    beautify : false,
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                                '<%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                },
+                files : {
+                    '<%= dirs.dest %>/listPage_min_<%= pkg.version %>.js' : ['<%= dirs.dest%>/listPage.js'],
+                    '<%= dirs.dest %>/listPage_m_min_<%= pkg.version %>.js' : ['<%= dirs.dest%>/listPage_m.js'],
+                }
+            }
+        }
+    });
+
+
+    //Load
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //Default Task(s)
+    grunt.registerTask('default' , ['concat','uglify']);
+
+    //grunt.registerTask('mc' , ['concat']);
+};

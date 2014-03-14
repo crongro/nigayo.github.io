c09e8e0e1afe37110633cbc8ee3a5056bec10db2
jisu youn <jisu.youn@nhn.com>
2014-03-14 11:05:07 +0900

    add testcode

--- src/js/list_m.js
+++ src/js/list_m.js
@@ -1,1 +1,10 @@
 
+function isMobile() {
+      function isMobileAgent() {
+        return !! (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+      }
+      function isMobileWidth() {
+        return !! ($Document(document).clientSize().width < 401);
+      }
+      return !! (isMobileAgent() && isMobileWidth());
+}

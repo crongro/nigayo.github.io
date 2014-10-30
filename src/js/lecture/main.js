caed6e3a713303323313d4043f97d20ba576f5ce
jisu youn <jisu.youn@nhn.com>
2014-10-30 19:02:39 +0900

    apply scroll navigation movement and etc..

--- src/js/lecture/main.js
+++ src/js/lecture/main.js
@@ -1,13 +1,14 @@
 require.config({
     baseUrl:"/src",
     paths : {
-    	jindo : "lib/js/jindo"
+    	jindo : "lib/js/jindo",
+    	common : "js/lecture/common"
     },
     waitSeconds: 15
 });
 
 require([
-    "js/lecture/view",
+    "js/lecture/view"
 ], function (oIntro) {
 	console.log("called main callback function");
 	oIntro.init();
--- src/js/lecture/view.js
+++ src/js/lecture/view.js
@@ -1,9 +1,41 @@
-define(["jindo"] , function(jindo) {
+//define(["jindo"] , function(jindo) {
+define(["common"] , function(oCommon) {
 	var _innerVar;
+	var _welNav;
+
 
 	function _init() {
-		console.log("initfunction");
-		console.log("jindo test 2", jindo)
+		_initElements();
+		_onEvents();
+	}
+
+	function _initElements() {
+		_welMainBody = jindo.$Element("mainBody");
+		_welNav = _welMainBody.query(".left_nav");
+	}
+
+	function _onEvents () {
+		//TODO
+		//_welNav.attach('')
+		var _welWindow = jindo.$Element(window);
+		var _nNavTopPos = _welMainBody.offset().top; //220
+
+		var nScrollStart = 0;
+		_welWindow.attach("scroll", function(we) {
+			_nScrollY = we.currentElement.scrollY;
+
+			if(_nScrollY < _nNavTopPos)  return;
+
+			var _nNowScrollTime = new jindo.$Date(Date.now());
+			nScrollStart = _nNowScrollTime;
+
+			setTimeout(function(){
+				var _afterTime = new jindo.$Date(Date.now());
+					if((nScrollStart.compare(_afterTime)) > 500) {
+						console.log("1초 동안 scroll 변화없었음",nScrollStart.time(), _afterTime.time(), nScrollStart.compare(_afterTime));
+					}
+			},500);
+		});
 	}
 
 	return {

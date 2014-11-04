bc3d8f94cb1d43b00534cc1bdc609ec1c3e52437
younjisu <niganiga@naver.com>
2014-11-04 18:49:30 +0900

    template 1차완성

--- _runner/makeLectureHTML.js
+++ _runner/makeLectureHTML.js
@@ -1,25 +1,29 @@
 var fs = require('fs');
 
 var inFile = "../page/lecture/uib/template.html";
-var outFile = "./result.html";
 
 (function() {
 
 	//설정파일 읽기
     var htData = require('./data/webuibasic.js').data;
+    var htData_len = htData.length;
 
     fs.readFile(inFile, 'utf8', function(err,data){
     	if(err) return console.log("file error");
 
-    	for(var value in htData[0]) {
-    		var _sReg = '<!--%'+value +'-->';
-	    	data = data.replace(_sReg, htData[0][value]);
+    	for(var i=0; i<htData_len; i++) {
+
+    		var result = data;
+
+	    	for(var value in htData[i]) {
+	    		var _sReg = '<!--%'+value +'-->';
+		    	result = result.replace(_sReg, htData[i][value]);
+	    	}
+
+	    	fs.writeFile("./"+(i+1)+".html", result, 'utf8', function(err) {
+	    		if(err) return console.log('error write', err);
+	    	});
     	}
-
-
-    	fs.writeFile(outFile, data , 'utf8', function(err) {
-    		if(err) return console.log('error write', err);
-    	});
     });
 
 })();
---
+++ _runner/move.sh
@@ -0,0 +1,1 @@
+cp *.html /Users/nhn/src/github/gitblog/nigayo.github.io/page/lecture/uib/

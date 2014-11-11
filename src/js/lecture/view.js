efe5b4d16b7ffbc88416ef89e737c9a41fc4b34a
younjisu <niganiga@naver.com>
2014-11-11 16:58:52 +0900

    test

--- src/js/lecture/view.js
+++ src/js/lecture/view.js
@@ -48,7 +48,7 @@ define(["common", "data"] , function(oCommon, oData) {
 	function _onEvents () {
 
 
-		if( oUtil.isMobile() )  { 
+		//if( oUtil.isMobile() )  { 
 			_welAllMenu.attach("touchEnd" , function() {
 				if(_welNavMobile.css("display") === "none") _welNavMobile.css("display", "block");
 				we.stop(jindo.$Event.CANCEL_ALL);
@@ -60,11 +60,11 @@ define(["common", "data"] , function(oCommon, oData) {
 				we.stop(jindo.$Event.CANCEL_ALL);
 			});
 
-		} else {
+		//} else {
 			_welWindow.attach("scroll", function(we) {
 				_scrollMonitorHandler(we);
 			});
-		}
+		//}
 
 	}
 
